3fb43588cc5386e1c1b26ca33678267bddd09c34
Sarthak Moriya <sarthak8544@gmail.com>
2023-03-13 12:39:09 +0530

    Delete server directory

--- server/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import * as Controller from '../controllers/auth.js'
-
-const router=new Router();
-
-router.post('/login',Controller.login)
-
-export default router
